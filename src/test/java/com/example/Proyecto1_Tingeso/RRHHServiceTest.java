76fbd4b019c15acd2f850ea535d9814ebafe9ab9
Joakin Roa <joakin.roa@usach.cl>
2022-10-05 15:45:03 -0300

    arreglando problemas de tests2

--- src/test/java/com/example/Proyecto1_Tingeso/RRHHServiceTest.java
+++ src/test/java/com/example/Proyecto1_Tingeso/RRHHServiceTest.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
+
 @TestPropertySource(locations = "classpath:test.properties")
 public class RRHHServiceTest {
 
